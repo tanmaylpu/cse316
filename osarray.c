0273d4a10b3d5d53df0791c3974a280499fe302c
Tanmay Bhattacharjee <tanmay.lpu@gmail.com>
2019-04-05 11:30:20 +0100

    Sorting Function

---
+++ osarray.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+    void main ()
+    {
+ 
+        int number[30];
+ 
+        int i, j, a, n;
+        printf("Enter the number of Students:\n");
+        scanf("%d", &n);
+ 
+        for (i = 0; i < n; ++i) 
+		{
+        printf("Enter the Gift Price for student %d\n", i+1);
+	    scanf("%d", &number[i]);
+		}
+ 
+        /*  sorting begins ... */
+ 
+        for (i = 0; i < n; ++i) 
+        {
+            for (j = i + 1; j < n; ++j) 
+            {
+                if (number[i] < number[j]) 
+                {
+                    a = number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+                }
+            }
+        }
